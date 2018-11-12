c05b029640963e116c49b69fedcf294b2ba26c3e
duanxiaoqiu <Dxq#5201314>
2018-11-12 18:38:10 +0800

    修改spring 4.0

--- DataWeb/src/main/java/com/xq/web/api/IndexApiController.java
+++ DataWeb/src/main/java/com/xq/web/api/IndexApiController.java
@@ -3,6 +3,7 @@ package com.xq.web.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Package: com.xq.web.controller.com.xq.web.api
@@ -12,9 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Time: 18:44
  * Description:
  */
-@Controller
 @RequestMapping("/api/index")
+@Controller
 public class IndexApiController {
+
+    @RequestMapping("/test.ajax")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+
 }
 
 
